c4b979356c75df4db7a75c73f2fd676c47794a26
Michael Harris <mwharris7@gmail.com>
2020-06-13 17:26:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,15 +8,16 @@ class Triangle
   end
 
   def kind
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-        puts error.message
-      end
+
+  end
+
+  def valid?
+    if a <= 0 || b <= 0 || c <= 0
+      return false
+    elsif a + b > c || b + c > a || a + c > b
+      return false
     else
-      person.partner = self
+      return true
     end
   end
 
@@ -26,3 +27,17 @@ class Triangle
     end
   end
 end
+
+
+# def kind
+#   self.partner = person
+#   if person.class != Person
+#     begin
+#       raise TriangleError
+#     rescue PartnerError => error
+#       puts error.message
+#     end
+#   else
+#     person.partner = self
+#   end
+# end
