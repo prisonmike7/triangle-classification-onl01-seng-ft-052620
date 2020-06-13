e46ea235da117d650aec3fc222a85c3111829631
Michael Harris <mwharris7@gmail.com>
2020-06-13 17:38:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,7 +24,7 @@ class Triangle
   def valid?
     if @a <= 0 || @b <= 0 || @c <= 0
       return false
-    elsif @a + @b < @c || @b + @c < @a || @a + @c < @b
+    elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
       return false
     else
       return true
