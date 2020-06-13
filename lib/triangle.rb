d76edade1dc7de0741443fdbd501173d3c897a6f
Michael Harris <mwharris7@gmail.com>
2020-06-13 17:18:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b ,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    self.partner = person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle input invalid!"
+    end
+  end
 end
